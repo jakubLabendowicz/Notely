b133e61fb784414b2a33b4c320db70daddaf348d
jakubLabedowicz <jalabedowicz@gmail.com>
2024-05-24 11:08:31 +0200

    edit logs

--- server/src/middlewares/ErrorMiddleware.js
+++ server/src/middlewares/ErrorMiddleware.js
@@ -1,12 +1,10 @@
 const handleError = (error, req, res, next) => {
     console.log("Error: ", error)
-    console.log("Request: ", req.body)
     res.status(error.status || 500).send({ result: { message: error.message || "Internal server error", type: "error", status: error.status || 500, instance: req.originalUrl } })
 }
 
 const handleNotFound = (req, res, next) => {
     console.log("Not found error")
-    console.log("Request: ", req.body)
     res.status(404).send({ result: { message: "Not found", type: "error", status: 404, instance: req.originalUrl } })
 }
 
